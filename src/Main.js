32ef2e9e59fb160e25124ec1548fe8a983f27f56
sophearahsp <sophearahspsp@gmail.com>
2020-07-06 13:16:49 -0700

    Set up constructor, created input elements, display list of names
    constructor: state object and connecting event handlers
    input elements: form with text input and a button and their event handlers
    display list of names: mapping each string to a <li>

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react'
 
 // objective: display list
+
 export default class Main extends Component {
     
     constructor(props){
         super(props);
-        console.log(props.listID+" is the id from main")
+        this.state = {
+            babyData:{
+                listID: props.listID,
+                // TODO check database to see if there are names
+                names: ["somally", "darin", "sophearah"]
+            },
+            nameTextBox: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleNameTextBoxChange = this.handleNameTextBoxChange(this);
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("pressed submit button");
+    }
+
+    handleNameTextBoxChange = (event) => {
+        console.log("name in textbox changed");
     }
 
     render() {
         return (
             <div>
-                <form>
-
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type = "text"
+                        value = {this.state.nameTextBox}
+                        onChange = {this.handleNameTextBoxChange}
+                    />
+                    
+                    <button type="submit">Submit</button>
                 </form>
+
+                {// map names
+                this.state.babyData.names.map((name) => (
+                    <li key={name}>{name}</li>
+                ))
+                }
                 
             </div>
         )
