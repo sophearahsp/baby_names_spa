9c22b31febacac37aadabb1194b2f3b689d611c1
sophearahsp <sophearahspsp@gmail.com>
2020-07-09 00:23:42 -0700

    Created new listID on database

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import React, {Component} from 'react';
 import Main from "./Main"
 
 export default class App extends Component {
-  generateNewID = () => {
-    //TODO: generate ID on API
-    let genID = Math.random().toString(36).slice(2);
-    this.setState({listID: genID})
-    window.history.pushState({},"","/?list_id=" + genID);
-  }
-
   constructor(props){
     super(props);
     this.state = {
@@ -22,23 +15,33 @@ export default class App extends Component {
 
     if ((href.startsWith(protocol + "//" + host + "/?list_id=")) && // if has something as id
       (href.split('=')[1].match(/^[a-zA-Z0-9]{12}$/g)) && // if id is alphanumeric and 12 characters
-      (this.VerifyUrlIdentification(href.split('=')[1])) // it is verified to be in the database
+      (this.verifyUrlIdentification(href.split('=')[1])) // it is verified to be in the database
     ){
       this.state.listID = href.split('=')[1];
     }else{
-      this.generateNewID();
+      this.newUrlIdentification();
     }
+
+    
   }
 
-  async VerifyUrlIdentification(toVerify){
+  async verifyUrlIdentification(toVerify){
     const API = 'http://localhost:3001/api/v1/url_identifications/';
     const response = await fetch(API);
-    console.log(response)
     const data = await response.json();
-    
+
     if (data.find(datum => datum.identification === toVerify)){
       return true
     }else{return false}
+  }
+
+  async newUrlIdentification(){
+    const API = 'http://localhost:3001/api/v1/url_identifications/new';
+    const response = await fetch(API);
+    const data = await response.json();
+
+    this.state = {listID: data.identification, dbId: data.id}
+    window.history.pushState({},"","/?list_id=" + this.state.listID);
   }
 
   render(){
