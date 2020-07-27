f963fbdadba3a184bcf260bc60c4cbc2c7c56bd3
sophearahsp <sophearahspsp@gmail.com>
2020-07-27 08:02:56 -0700

    same as the other reworked app.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ import Main from "./Main"
 //const baseAPI = 'http://localhost:3001/api/v1/';
 const baseAPI = 'https://floating-headland-40405.herokuapp.com/api/v1/';
 
-// const initGet = {
-//   method: 'GET',
-//   accept: 'application/json',
-//   cache: 'default',
-//   headers: {}
-// };
-
 const href = window.location.href;
 const protocol = window.location.protocol;
 const host = window.location.host;
@@ -24,62 +17,56 @@ export default class App extends Component {
       listID: "",
       dbID: -1
     }
+    
+    // if has url has an id
+    const checkUrlHasID = href.startsWith(protocol + "//" + host + "/?list_id=")
+    // and if id is alphanumeric and has 12 characters
+    const checkAlphanum12Char = href.includes("=") ? href.split('=')[1].match(/^[a-zA-Z0-9]{12}$/g) : false;
 
-    // check if user came in with link
-    if ((href.startsWith(protocol + "//" + host + "/?list_id=")) && // if has something as id
-      (href.split('=')[1].match(/^[a-zA-Z0-9]{12}$/g)) && // if id is alphanumeric and 12 characters
-      (this.verifyUrlIdentification(href.split('=')[1])) // it is verified to be in the database
-    ) {
-      // set state of listID & dbID
-      this.state = {listID: href.split('=')[1]};
-      
-      this.getDbID()
-        .then(db => {
-          if (db === undefined){
-            this.newUrlIdentification()
-              .then(urlID => {this.setState({
-                listID: urlID.identification,
-                dbID: urlID.id})})
-              .then(x => window.history.pushState({}, "", "/?list_id=" + this.state.listID));
-          }else{
-            this.getDbID()
-              .then((db) => this.setState({dbID: db.id}));
-          }
+    // if url has id && its valid
+    if (checkUrlHasID && checkAlphanum12Char){
+      this.getThisIdFromDb(href.split('=')[1])
+        .then(idObj => {
+          //if exists, sets state
+          if (idObj){
+            this.setState({
+              listID: idObj.identification,
+              dbID: idObj.id});
+          }else{this.setIdChangeUrl()}//otherwise create new id
         })
-    } else { // else new ID
-      this.newUrlIdentification()
-        .then(urlID => {this.setState({
-          listID: urlID.identification,
-          dbID: urlID.id})})
-        .then(x => window.history.pushState({}, "", "/?list_id=" + this.state.listID));
-    }
+    } else{this.setIdChangeUrl()}//otherwise create new id
   }
 
-  async verifyUrlIdentification (toVerify) {
-    const response = await fetch(baseAPI+"url_identifications/",initGet);
-    const data = await response.json();
-    if (data.find(datum => datum.identification === toVerify)) {
-      return true
-    } else { return false }
-  }
- 
-  async getDbID(){
-    const response = await fetch(baseAPI+"url_identifications/",initGet);
-    const data = await response.json();
-    const urlIdDatabase = data.find(datum => datum.identification === this.state.listID)
-    if (urlIdDatabase === undefined){
-      return undefined
-    }
-    return urlIdDatabase
+  //gets new id from db and sets state and change current url
+  setIdChangeUrl = () => {
+    this.getNewUrlIdFromDb()
+      .then(urlID => {this.setState({
+        listID: urlID.identification,
+        dbID: urlID.id})})
+      .then(x => window.history.pushState({}, "", "/?list_id=" + this.state.listID));
   }
 
-  async newUrlIdentification() {
+  //looks for urlidentification with given identification in database
+  getThisIdFromDb = async (idFromUrl) => {
+    const response = await fetch(baseAPI+"url_identifications/",initGet);
+    const data = await response.json();
+    //if id exists in database
+    if (data.some(datum => datum.identification === idFromUrl)) {
+      let datum = data.find(datum => datum.identification === idFromUrl);
+      console.log(datum)
+      return datum//return urlidentification object
+    }else{return null}//otherwise return false
+  }
+
+  //create new id in database and return urlidentification object
+  getNewUrlIdFromDb = async () => {
     const response = await fetch(baseAPI + "url_identifications/new", initGet);
     const data = await response.json();
     return data
   }
 
   render() {
+    console.log("new")
     if (!((this.state.dbID === undefined)||(this.state.dbID === -1))){
       return (
         <div>
@@ -89,7 +76,7 @@ export default class App extends Component {
     }else{
       return (
         <div>
-          waiting for data
+          Loading data...
         </div>
       )
     }
